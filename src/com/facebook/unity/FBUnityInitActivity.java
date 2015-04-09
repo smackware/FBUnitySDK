aa60f20d1fb771a62aaf0b447a4c334a029813d4
Lital Natan <litaln@gmail.com>
2015-04-09 14:40:31 +0300

    Added init activity

---
+++ src/com/facebook/unity/FBUnityInitActivity.java
@@ -0,0 +1,14 @@
+package com.facebook.unity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+import com.facebook.Session;
+
+public class FBUnityInitActivity extends Activity{
+	public static final String LOGIN_PARAMS = "login_params";
+	public static final String APP_ID_PARAMS = "app_id";
+
+}
